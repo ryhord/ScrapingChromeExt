69135672ebb601aa99086868f5e37aa6631070e3
Ryan Hord <ryhord13@gmail.com>
2018-07-17 16:32:36 -0400

    json repsonse displays in popup.html

--- scraping.js
+++ scraping.js
@@ -10,12 +10,41 @@ function run() {
         function (foundTabs) {
             if (foundTabs.length > 0) {
                 url = foundTabs[0].url; // <--- this is what you are looking for
-                alert(selector + " " + url);
                 /*alert(selector + " " + url);*/
+                var jsonReqUrl = "https:\/\/scrapable.herokuapp.com/results/api?url=" + url + "&selector=" + selector;
+                /* alert(jsonReqUrl); */
+                var jsonRes = Get(jsonReqUrl);
+                /* alert(jsonRes); */
+
+                document.documentElement.innerHTML = '';
+                document.documentElement.innerHTML = jsonRes;
+
+                //f
+
+                //ar element = document.getElementById("element-id");
+                //element.parentNode.removeChild(element);
+
+
+
+
+
+
+
+
+
             } else {
                 alert("there's no window or no selected tab");
             }
         })
     
 
+
+    //display json response
+}
+
+function Get(yourUrl) {
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET", yourUrl, false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
 }
